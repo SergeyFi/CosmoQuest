0d2734fe9f1fb24f9ad368e719715e5cfbd96031
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2021-07-11 20:50:32 +0300

    Add health component

---
+++ Source/CosmoQuest/Components/HealthComponentC.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CosmoQuest/Components/HealthComponentC.h"
+
+// Sets default values for this component's properties
+UHealthComponentC::UHealthComponentC()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+void UHealthComponentC::RemoveHealth(float Damage)
+{
+	Health -= Damage;
+
+	if (Health < 0.0f)
+	{
+		Health = 0.0f;
+	}
+}
+
+
+// Called when the game starts
+void UHealthComponentC::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UHealthComponentC::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/CosmoQuest/Components/HealthComponentC.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponentC.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(IsBlueprintBase = "true") )
+class COSMOQUEST_API UHealthComponentC : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponentC();
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveHealth(float Damage);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Health = 100.0f;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
