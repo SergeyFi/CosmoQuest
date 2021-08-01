4f3ce3e6c90a49922c4b884155d0cc0122e9352f
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2021-08-01 14:35:17 +0300

    Add base spawn point class

---
+++ Source/CosmoQuest/Components/SpawnPointC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ArrowComponent.h"
+#include "SpawnPointC.generated.h"
+
+/**
+ * 
+ */
+UCLASS( ClassGroup=(Custom), meta=(IsBlueprintBase = "true") )
+class COSMOQUEST_API USpawnPointC : public UArrowComponent
+{
+	GENERATED_BODY()
+	
+};
